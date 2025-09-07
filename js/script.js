2040f2786f2358b42d974245c612f80b3ebd0452
Ederson <jjarbitrobal@gmail.com>
2025-09-07 14:48:43 -0500

    habitaciones imagenes

--- js/script.js
+++ js/script.js
@@ -6,3 +6,30 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+let index = 0;
+
+function showSlide(n) {
+    const slides = document.querySelectorAll('.carousel-item');
+    if (n >= 4) index = 0;
+    if (n < 0) index = slides.length - 1;
+
+    for (let slide of slides) {
+        slide.style.transform = `translateX(-${index * 100}%)`;
+    }
+}
+
+function nextSlide() {
+    index++;
+    showSlide(index);
+}
+
+function prevSlide() {
+    index--;
+    showSlide(index);
+}
+
+// slide inicial
+showSlide(index);
+
+//proximo slide
+setInterval(nextSlide, 8000);
